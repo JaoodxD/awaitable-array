16dae3f19903ec59f535dcf9af2a8cee4c8cbfdc
JaoodxD <jaood96@gmail.com>
2024-05-16 01:58:49 +0300

    Remove debug try catch

--- approaches/null-mutations-no-await.mjs
+++ approaches/null-mutations-no-await.mjs
@@ -2,13 +2,9 @@ export default function nullMutationsNoAwait (resolve) {
   this.then = null
   let counter = this.length
   for (let i = 0; i < this.length; i++) {
-    try {
-      this[i].then((value) => {
-        this[i] = value
-        if(--counter === 0) resolve(this)
-      })
-    } catch (error) {
-      console.log(this, error)
-    }
+    this[i].then(value => {
+      this[i] = value
+      if (--counter === 0) resolve(this)
+    })
   }
 }
