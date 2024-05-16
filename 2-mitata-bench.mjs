07f2db88b345d986a88b21d23ef12f02a6d5776e
JaoodxD <jaood96@gmail.com>
2024-05-16 12:56:24 +0300

    Add result assertion

--- 2-mitata-bench.mjs
+++ 2-mitata-bench.mjs
@@ -1,10 +1,12 @@
-import { run, bench, group,baseline } from 'mitata'
+import { run, bench, group, baseline } from 'mitata'
+import { strictEqual } from 'node:assert'
 import arrayLikeObject from './approaches/array-like-object.mjs'
 import nullMutations from './approaches/null-mutations.mjs'
 import nullMutationsNoDelete from './approaches/null-mutations-no-delete.mjs'
 import oldArray from './approaches/old-array.mjs'
 import nullMutationsNoAsyncAwait from './approaches/null-mutations-no-async-await.mjs'
 import nullMutationsNoAwait from './approaches/null-mutations-no-await.mjs'
+import nullNoAwait from './approaches/null-no-await.mjs'
 
 const thenables = [
   arrayLikeObject,
@@ -12,18 +14,23 @@ const thenables = [
   nullMutationsNoDelete,
   oldArray,
   nullMutationsNoAsyncAwait,
-  nullMutationsNoAwait
+  nullMutationsNoAwait,
+  nullNoAwait
 ]
 
 group('all together', () => {
   baseline('Promise.all', async () => {
     const [a, b] = await Promise.all([Promise.resolve(1), Promise.resolve(2)])
+    strictEqual(a, 1)
+    strictEqual(b, 2)
   })
   for (const thenable of thenables) {
     bench(thenable.name, async () => {
       Array.prototype.then = thenable
 
       const [a, b] = await [Promise.resolve(1), Promise.resolve(2)]
+      strictEqual(a, 1)
+      strictEqual(b, 2)
     })
   }
 })
